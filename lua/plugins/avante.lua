2fa9a94f16f1b3afc10bfd5f048490d2146ebb2c
RaedN0 <marcel.schoerghuber@gmail.com>
2025-05-26 14:44:21 +0200

    Some changes

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,30 +1,24 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
+  version = false,
   opts = {
-    -- add any opts here
-    -- for example
     provider = "gemini",
     gemini = {
       model = "gemini-2.5-pro-preview-05-06",
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
     "echasnovski/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -6,11 +6,8 @@ return {
         java = {
           format = {
             settings = {
-              ["org.eclipse.jdt.core.formatter.alignment_for_parameters_in_method_declaration"] = 80,
-              ["org.eclipse.jdt.core.formatter.alignment_for_throws_clause_in_method_declaration"] = 80,
-              ["org.eclipse.jdt.core.formatter.continuation_indentation"] = 1,
-              ["org.eclipse.jdt.core.formatter.insert_new_line_before_opening_brace_in_method_declaration"] = "insert",
-              ["org.eclipse.jdt.core.formatter.lineSplit"] = 100,
+              url = "/Users/mschoerghuber/.config/nvim/java-formatter.xml",
+              profile = "Project",
             },
           },
           completion = {
